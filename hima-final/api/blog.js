80b1aef3033c3527dd5a443fbc27b035111762df
Hima Tech RCM <info@himatechrcm.com>
2026-09-18 19:44:13 +0500

    Fix: Convert blog API to CommonJS for Vercel compatibility

--- hima-final/api/blog.js
+++ hima-final/api/blog.js
@@ -36,7 +36,7 @@ async function commitBlog(newPosts, sha, message) {
   return res.json();
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -71,4 +71,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
